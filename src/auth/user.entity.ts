51eab51c832a3b27f362b0d4c678a9e66674b87d
xiaosasori <xiaosasori@gmail.com>
2021-12-07 17:01:27 +0900

    authorization

--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -1,4 +1,5 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Task } from 'src/tasks/task.entity';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class User {
@@ -10,4 +11,8 @@ export class User {
 
   @Column()
   password: string;
+
+  // whenvever we fetch user from db, we also get tasks
+  @OneToMany((_type) => Task, (task) => task.user, { eager: true })
+  tasks: Task[];
 }
